a0547f8063965a83d008683bbf774abac070a905
ABO0069 <ayush.biswas@g.bracu.ac.bd>
2024-12-09 21:19:20 +0600

    case sensitivity issue?

--- biblioteca/src/App.tsx
+++ biblioteca/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './layouts/navbar-footer/Navbar';
+import { Navbar } from './layouts/navbar-footer/navbar';
 import { Footer } from './layouts/navbar-footer/Footer';
 import { HomePage } from './layouts/home/HomePage';
 export const App = () => {
