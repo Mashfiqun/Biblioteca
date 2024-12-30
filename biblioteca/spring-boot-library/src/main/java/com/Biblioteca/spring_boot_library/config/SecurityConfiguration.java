1db8b1a1150d78c9391ce933f71799d898a600e1
ABO0069 <ayush.biswas@g.bracu.ac.bd>
2024-12-30 22:51:22 +0600

    checkout functionality added

--- biblioteca/spring-boot-library/src/main/java/com/Biblioteca/spring_boot_library/config/SecurityConfiguration.java
+++ biblioteca/spring-boot-library/src/main/java/com/Biblioteca/spring_boot_library/config/SecurityConfiguration.java
@@ -9,17 +9,19 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
- 
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfiguration {
- 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
- 
-        
+
+        // Disable Cross Site Request Forgery
         http.csrf(AbstractHttpConfigurer::disable);
- 
-        
+
+        // Protect endpoints at /api/<type>/secure
         http.authorizeHttpRequests(configurer ->
                         configurer
                                 .requestMatchers("/api/books/secure/**",
@@ -27,19 +29,20 @@ public class SecurityConfiguration {
                                         "/api/messages/secure/**",
                                         "/api/admin/secure/**")
                                 .authenticated().anyRequest().permitAll())
-                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
- 
- 
-        
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        // Add CORS filters
         http.cors(Customizer.withDefaults());
- 
-        http.setSharedObject(ContentNegotiationStrategy.class,
-                new HeaderContentNegotiationStrategy());
- 
-        
+
+        //  Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+
+        // Force a non-empty response body to make unauthorized response body more friendly.
         Okta.configureResourceServer401ResponseBody(http);
- 
+
         return http.build();
     }
- 
+
 }
