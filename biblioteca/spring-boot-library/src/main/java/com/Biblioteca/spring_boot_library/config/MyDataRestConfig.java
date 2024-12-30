75ab2f38d209c649eb37f09c71da7cb2c47f069b
Mehedi Hassan <mhalif002@gmail.com>
2024-12-30 15:09:43 +0600

    Added reviews

--- biblioteca/spring-boot-library/src/main/java/com/Biblioteca/spring_boot_library/config/MyDataRestConfig.java
+++ biblioteca/spring-boot-library/src/main/java/com/Biblioteca/spring_boot_library/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.Biblioteca.spring_boot_library.entity.Book;
+import com.Biblioteca.spring_boot_library.entity.Review;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer { 
@@ -17,8 +18,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
         config.exposeIdsFor(Book.class);
-
+        config.exposeIdsFor(Review.class);
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
